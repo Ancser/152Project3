4a773356b91710ab67ee629e61798ddca8184944
Ancser <ancser24@gmail.com>
2024-12-01 12:23:43 -0800

    stopWait sender + customed receiver

--- docker/stopWait.py
+++ docker/stopWait.py
@@ -13,26 +13,27 @@ with open('file.mp3', 'rb') as f:
 packets = [data[i:i+MESSAGE_SIZE] for i in range(0, len(data), MESSAGE_SIZE)]
 
 # make udp socklet
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udpSocket:
     SERVER_ADDRESS = ('127.0.0.1', 5001)  # receiver address
 
     for seq_id, packet in enumerate(packets):
         # create package
         seq_id_bytes = int.to_bytes(seq_id, SEQ_ID_SIZE, byteorder='big', signed=True)
-        udp_packet = seq_id_bytes + packet
+        udpPacket = seq_id_bytes + packet
 
         # wait ack
         while True:
             try:
-                # 发送数据包
-                udp_socket.sendto(udp_packet, SERVER_ADDRESS)
+                # send package
+                udpSocket.sendto(udpPacket, SERVER_ADDRESS)
                 print(f"Sent packet {seq_id}")
 
-                # 等待 ACK
-                udp_socket.settimeout(2)  # 设置超时时间
-                ack, _ = udp_socket.recvfrom(PACKET_SIZE)
+                # Wait for ack
+                # set timeout time
+                udpSocket.settimeout(2)
+                ack, _ = udpSocket.recvfrom(PACKET_SIZE)
 
-                # 检查 ACK
+                # check for ack
                 ack_id = int.from_bytes(ack[:SEQ_ID_SIZE], byteorder='big', signed=True)
                 if ack_id == seq_id + 1:
                     print(f"Received ACK for packet {seq_id}")
@@ -41,6 +42,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
                 print(f"Timeout for packet {seq_id}, resending...")
 
     # send end signal
-    fin_packet = int.to_bytes(-1, SEQ_ID_SIZE, byteorder='big', signed=True) + b'==FINACK=='
-    udp_socket.sendto(fin_packet, SERVER_ADDRESS)
+    finPacket = int.to_bytes(-1, SEQ_ID_SIZE, byteorder='big', signed=True) + b'==FINACK=='
+    udpSocket.sendto(finPacket, SERVER_ADDRESS)
     print("Sent FINACK")
