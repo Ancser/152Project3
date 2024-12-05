b11cf4f4625c33ea31c29301876d37c169ca3f69
Ancser <ancser24@gmail.com>
2024-12-05 00:02:17 -0800

    Update t.py

--- docker/t.py
+++ docker/t.py
@@ -20,7 +20,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udpSocket:
     SERVER_ADDRESS = ('127.0.0.1', 5001)
     udpSocket.setblocking(False)
 
-    # Metric and TCP Variables
     startTime = time.time()
     totalRetransmission = 0
     cwnd = 1
@@ -30,7 +29,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udpSocket:
     sentTime = {}
 
     while baseIndex < len(packets):
-        # Send new packets within the current window
         while newIndex < baseIndex + cwnd and newIndex < len(packets):
             SeqID = newIndex
             sizeSeqID = SeqID * MESSAGE_SIZE
@@ -40,7 +38,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udpSocket:
             print(f"Sent packet ID [{sizeSeqID}] ({len(packets[SeqID])} bytes) <<<")
             newIndex += 1
 
-        # Check for ACKs
         received, _, _ = select.select([udpSocket], [], [], TIMEOUT)
         if received:
             ack, _ = udpSocket.recvfrom(PACKET_SIZE)
@@ -48,31 +45,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udpSocket:
             SeqID = sizeAckID // MESSAGE_SIZE
             print(f"Returning ACK ID [{sizeAckID}] >>>")
 
-            # Update baseIndex and congestion window
-            if SeqID >= baseIndex:
-                baseIndex = SeqID + 1
-                if cwnd < ssthresh:  # Slow start phase
-                    cwnd *= 2
-                else:  # Congestion avoidance
-                    cwnd += 1
+            if SeqID == baseIndex:  # Valid ACK for the expected packet
+                baseIndex += 1
+                if cwnd < ssthresh:
+                    cwnd *= 2  # Slow start
+                else:
+                    cwnd += 1  # Congestion avoidance
             else:
-                print(f"Warning: Received duplicate ACK for packet ID [{sizeAckID}]")
+                print(f"Warning: Received unexpected ACK ID [{sizeAckID}]")
 
         else:
-            # Timeout: retransmit all packets in the current window
+            # Timeout handling: Retransmit all packets in the current window
             print(f"Timeout: Retransmitting packets in window [{baseIndex}:{newIndex}]")
             ssthresh = max(cwnd // 2, 1)
-            cwnd = 1  # Tahoe: Reset cwnd to 1
+            cwnd = 1
             totalRetransmission += 1
             for SeqID in range(baseIndex, newIndex):
                 sizeSeqID = SeqID * MESSAGE_SIZE
-                if sizeSeqID in sentTime and (time.time() - sentTime[sizeSeqID]) > TIMEOUT:
-                    udpPacket = int.to_bytes(sizeSeqID, SEQ_ID_SIZE, byteorder='big', signed=True) + packets[SeqID]
-                    udpSocket.sendto(udpPacket, SERVER_ADDRESS)
-                    sentTime[sizeSeqID] = time.time()
-                    print(f"Retransmitted packet ID [{sizeSeqID}] <<<")
+                udpPacket = int.to_bytes(sizeSeqID, SEQ_ID_SIZE, byteorder='big', signed=True) + packets[SeqID]
+                udpSocket.sendto(udpPacket, SERVER_ADDRESS)
+                sentTime[sizeSeqID] = time.time()
+                print(f"Retransmitted packet ID [{sizeSeqID}] <<<")
 
-    # Send FIN packet
     finPacket = int.to_bytes(-1, SEQ_ID_SIZE, byteorder='big', signed=True) + b'==FINACK=='
     udpSocket.sendto(finPacket, SERVER_ADDRESS)
     print("Sent FINACK signal")
